13210dbd8a1aa61e604f9f937159ca8ac2d3b337
Schluempfli <schluempfli.schluempfli.de>
2021-01-16 17:32:17 +0100

    interrupts for buttons

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,30 @@
 #include <Arduino.h>
-
 #include "WiFi.h"
 #include <HTTPClient.h>
+
+#define button1 17
+#define button2 23
+#define button3 34
+#define button4 35
  
 
 HTTPClient http;
 int code;
-// const char* ssid = "yourNetworkName";
-// const char* password = "yourNetworkPassword";
+const char* ssid = "yourNetworkName";
+const char* password = "yourNetworkPassword";
  
 void setup() {
- 
+  
+  pinMode(button1, INPUT_PULLUP);
+  pinMode(button2, INPUT_PULLUP);
+  pinMode(button3, INPUT_PULLUP);
+  pinMode(button4, INPUT_PULLUP);
+  digitalWrite(button1, HIGH);  //eigentlich nicht notwendig - aber funktioniert deutlich besser
+  digitalWrite(button2, HIGH);  //eigentlich nicht notwendig - aber funktioniert deutlich besser
+  digitalWrite(button3, HIGH);  //eigentlich nicht notwendig - aber funktioniert deutlich besser
+  digitalWrite(button4, HIGH);  //eigentlich nicht notwendig - aber funktioniert deutlich besser
   Serial.begin(9600);
- 
   WiFi.begin(ssid, password);
- 
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.println("Connecting to WiFi..");
