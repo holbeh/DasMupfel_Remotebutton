9fb5b693a0328b38e5004b9afe39c694c4ff47f0
Finn Behrens <me@kloenk.de>
2021-01-16 19:40:17 +0100

    move buttons to buttons.cpp

---
+++ include/buttons.h
@@ -0,0 +1,7 @@
+#ifndef __BUTTONS_H_
+#define __BUTTONS_H_
+
+void detachAllInterupts();
+void attachAllInterupts();
+
+#endif // __BUTTONS_H_
--- src/main.cpp
+++ src/main.cpp
@@ -2,57 +2,12 @@
 
 #include <WiFi.h>
 #include <HTTPClient.h>
-#include <shelly.h>
 
+#include <shelly.h>
+#include <buttons.h>
 
 #include "debug.h"
 
-volatile unsigned long alteZeit=0, entprellZeit=300;
-
-void deprell(button_t button){
-    if((millis() - alteZeit) > entprellZeit) { 
-    alteZeit = millis(); // letzte Schaltzeit merken     
-    Serial.printf("%d wurde betaetigt \n", (int)button); 
-  }
-}
-
-void detachAllInterupts();
-void attachAllInterupts();
-
-void interruptRoutine_LED_BAR_RIGHT() {
-  detachAllInterupts();
-  deprell(BUTTON_LED_BAR_RIGHT);
-  attachAllInterupts();
-}
-void interruptRoutine_LED_BAR_BACK() {
-  detachAllInterupts();
-  deprell(BUTTON_LED_BAR_BACK);
-  attachAllInterupts();
-}
-void interruptRoutine_CEILING_LED() {
-  detachAllInterupts();
-  deprell(BUTTON_CEILING_LED_1);
-  attachAllInterupts();
-}
-void interruptRoutine_HEATER_START() {
-  detachAllInterupts();
-  deprell(BUTTON_HEATER_START);
-  attachAllInterupts();
-}
-
-void attachAllInterupts(){
-  attachInterrupt((int)BUTTON_LED_BAR_RIGHT, interruptRoutine_LED_BAR_RIGHT, FALLING);
-  attachInterrupt((int)BUTTON_LED_BAR_BACK, interruptRoutine_LED_BAR_BACK, FALLING);
-  attachInterrupt((int)BUTTON_CEILING_LED_1, interruptRoutine_CEILING_LED, FALLING);
-  attachInterrupt((int)BUTTON_HEATER_START, interruptRoutine_HEATER_START, FALLING);
-}
-
-void detachAllInterupts(){
-  detachInterrupt((int)BUTTON_LED_BAR_RIGHT);
-  detachInterrupt((int)BUTTON_LED_BAR_BACK);
-  detachInterrupt((int)BUTTON_CEILING_LED_1);
-  detachInterrupt((int)BUTTON_HEATER_START);
-}
 
 //HTTPClient http;
 HTTPClient* http;
